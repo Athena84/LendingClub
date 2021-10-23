29c8cdb167da5547e55502213feb304df5dd7483
Athena84 <n.a.w.kreuk@gmail.com>
2021-10-23 15:41:10 +0200

    Clean data applications
    Clean rejected data file
    Combine rejected and accepted data

--- DataCleaner.R
+++ DataCleaner.R
@@ -1,7 +1,8 @@
 
 #Packages
 library(tidyverse)
-library(caret)
+library(stringi)
+library(lubridate)
 
 #Read the data
 accepted <- read.csv("./data/accepted_2007_to_2018Q4.csv", header = TRUE, sep = ",", stringsAsFactors = TRUE)
@@ -14,10 +15,30 @@ rejected <- read.csv("./data/rejected_2007_to_2018Q4.csv", header = TRUE, sep = 
 #apply(accepted, 2, unique)
 #ceiling(colMeans(is.na(accepted)) * 100)
 
-#Selecting key columns and removing missing value observations
+#Save cleaned data set for analysis on accepted loans
 selected_cols <- c("addr_state", "annual_inc", "dti", "emp_length", "fico_range_low", "fico_range_high", "grade", "id", "int_rate", "loan_amnt", "funded_amnt", "purpose", "sub_grade", "term", "issue_d", "loan_status", "settlement_status", "last_pymnt_d", "total_pymnt")
 cleaned_accepted <- accepted[ , selected_cols] %>%
   filter(., int_rate > 0)
-
-#Save cleaned data set
 write.csv(cleaned_accepted, "./data/cleaned_accepted.csv", row.names = FALSE)
+
+#Save combined cleaned version of rejected & accepted loans for analysis
+rejected <- rename(rejected,
+                   Risk.Score = Risk_Score
+                   )
+selected_rej_cols <- c("Amount.Requested", "Application.Date",  "Debt.To.Income.Ratio", "Employment.Length", "Risk.Score", "State") 
+rejected <- rejected[ , selected_rej_cols]
+rejected$Application.Date <- year(as.Date(rejected$Application.Date))
+rejected$Status <- "rejected"
+
+accepted <- rename(accepted,
+                   Amount.Requested = funded_amnt,
+                   Employment.Length = emp_length,
+                   State = addr_state,
+                   )
+accepted$Application.Date <- as.integer(stri_sub(accepted$issue_d, -4, -1))
+accepted$Risk.Score = (accepted$fico_range_high + accepted$fico_range_low) / 2
+accepted$Debt.To.Income.Ratio <- ifelse(accepted$application_type == "Individual", accepted$dti, accepted$dti_joint)
+accepted <- accepted[ , selected_rej_cols]
+accepted$Status <- "accepted"
+
+write.csv(rbind(accepted, rejected), "./data/cleaned_applications.csv", row.names = FALSE)
